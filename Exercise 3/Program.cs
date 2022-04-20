a5410f9113ba57091bbf169b55b70b9369585105
Eduard <dirol2016@list.ru>
2022-04-20 12:20:16 +0500

    3

---
+++ Exercise 3/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(100,999);
+Console.Write("Изначальное число: ");
+Console.WriteLine(number);
+int first = (number % 100) / 10;
+Console.Write("Конечное число: ");
+Console.WriteLine(first);
