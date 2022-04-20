feb72e4f60cbaae59543140eee9e8d42f779bc9e
Eduard <dirol2016@list.ru>
2022-04-20 12:16:04 +0500

    1

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите номер дня недели: ");
+int s = Convert.ToInt32(Console.ReadLine());
+ if(s == 1)
+ { Console.WriteLine("До выходный еще далеко!");
+ }
+ if(s == 2)
+ { Console.WriteLine("Пожалуй, нужно подождать еще!");
+ }
+ if(s == 3)
+ { Console.WriteLine("Ты прошел половину пути!");
+ }
+ if(s == 4)
+ { Console.WriteLine("Давай, еще сомвсем немного!");
+ }
+ if(s == 5)
+ { Console.WriteLine("Уже в предвкушении, да?)");
+ }
+ if(s == 6)
+ { Console.WriteLine("УРА!ВЫХОДНОЙ!");
+ }
+ if(s == 7)
+ { Console.WriteLine("УРА!ВЫХОДНОЙ!");
+ }
