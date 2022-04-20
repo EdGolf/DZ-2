7719967769afbd591ae745eb537ebda51f213881
Eduard <dirol2016@list.ru>
2022-04-20 12:18:59 +0500

    2

---
+++ Exercise 2/Program.cs
@@ -0,0 +1,18 @@
+﻿int numberA = 127;
+int numberB = (numberA % 100) / 10;
+
+
+  if(numberA > 100)
+  {
+    Console.Write("Изначальное число: "); 
+    Console.WriteLine(numberA);
+    Console.Write("Конечное число: ");
+    Console.WriteLine(numberB);
+
+  }
+  else
+  {
+    Console.Write("Изначальное число: ");
+    Console.WriteLine(numberA);
+    Console.Write("Третьей цифры нет"); 
+  }
